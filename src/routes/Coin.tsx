f5d4e0a493d72f4695be25767b2d61b4f158c4ae
신희성 <vvwv3092@gmail.com>
2023-07-02 18:16:26 +0900

    Add Router-States

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,10 +1,56 @@
-import { useParams } from "react-router-dom";
+import React, { useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import { styled } from "styled-components";
+
+const Container = styled.div`
+  padding: 0 20px;
+
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.accentColor};
+  font-size: 35px;
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
 
 type Params = {
   coinId: string;
 };
 
+interface RouterState {
+  state: {
+    name: string;
+  };
+}
+
 export const Coin = () => {
   const { coinId } = useParams<Params>();
-  return <div>Coin : {coinId}</div>;
+  const [loading, setLoading] = useState(true);
+
+  // page끼리 데이터를 주고 받아야하는 경우
+  // <Link to="/123" state={{ name: "Hello" }}> <- state를 전송
+  // const location = useLocation() <- 전송한 state를 받아옴
+  const { state } = useLocation() as RouterState;
+
+  return (
+    <Container>
+      <Header>
+        <Title>{state ? state.name : "Loading..."}</Title>
+      </Header>
+      {loading ? <Loader>Loading...</Loader> : null}
+    </Container>
+  );
 };
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -26,9 +26,10 @@ const Coin = styled.li`
   border-radius: 15px;
 
   a {
+    display: flex;
+    align-items: center;
     padding: 20px;
     transition: color 0.2s ease-in;
-    display: block;
   }
 
   &:hover {
@@ -46,6 +47,12 @@ const Title = styled.h1`
 const Loader = styled.span`
   text-align: center;
   display: block;
+`;
+
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
 `;
 
 interface CoinInterface {
@@ -83,7 +90,12 @@ export const Coins = () => {
         <CoinList>
           {coins.map((coin) => (
             <Coin key={coin.id}>
-              <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+              <Link to={`/${coin.id}`} state={{ name: coin.name }}>
+                <Img
+                  src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
+                />
+                {coin.name} &rarr;
+              </Link>
             </Coin>
           ))}
         </CoinList>
