3569e103b06f0e833be638d3e25965cd8999f31a
신희성 <vvwv3092@gmail.com>
2023-07-05 14:49:52 +0900

    PriceChart Add Chart

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,16 +1,78 @@
 import { useQuery } from "@tanstack/react-query";
 import { useOutletContext } from "react-router-dom";
 import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
 
 interface IChartProps {
   coinId: string;
 }
 
+interface IHistorical {
+  time_open: number;
+  time_close: number;
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
+  market_cap: number;
+}
+
 export const Chart = () => {
   const { coinId } = useOutletContext<IChartProps>();
-  const { isLoading, data } = useQuery(["ohlcv", coinId], () =>
+  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
     fetchCoinHistory(coinId)
   );
   console.log(data);
-  return <h1>Chart</h1>;
+  return (
+    <h1>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: "Price",
+              data: data?.map((price) => price.close),
+            },
+          ]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              height: 300,
+              width: 500,
+              background: "transparent",
+              toolbar: {
+                show: false,
+              },
+            },
+            grid: {
+              show: false,
+            },
+            stroke: {
+              curve: "smooth",
+              width: 4,
+            },
+            yaxis: {
+              show: false,
+            },
+            xaxis: {
+              axisTicks: {
+                show: false,
+              },
+              axisBorder: {
+                show: false,
+              },
+              labels: {
+                show: false,
+              },
+            },
+          }}
+        ></ApexChart>
+      )}
+    </h1>
+  );
 };
